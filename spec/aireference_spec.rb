3922a39fba23fed6f5a5b6abf13c8dfc59ce79fc
jcolnago <jessicacolnago@hotmail.com>
2012-04-05 14:28:23 -0300

    ...

--- spec/aireference_spec.rb
+++ spec/aireference_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 include MINT
 
-describe 'AUI' do
+describe 'AIReference with refers' do
   before :each do
     connection_options = { :adapter => "in_memory"}
     DataMapper.setup(:default, connection_options)
@@ -21,7 +21,35 @@ describe 'AUI' do
     end
     it 'should refer to correct object' do
       @a.refers.name.should == "target"
+    end
 
+    it 'should focus referred object' do
+      #Todo ask Sebastian about the functioning of AIReference
+      @a.process_event(:organize)
+      @a.refers.process_event(:organize)
+      @a.process_event(:present)
+      @a.refers.process_event(:present)
+      @a.process_event(:focus)
+      @a.refers.states.should == [:focused]
+    end
+
+  end
+
+  describe 'AIReference without refers' do
+    before :each do
+      connection_options = { :adapter => "in_memory"}
+      DataMapper.setup(:default, connection_options)
+      #    DataMapper.setup(:default, { :adapter => "rinda",:local =>Rinda::TupleSpace.new})
+      MINT::AIReference.new(:name=>"ref").save
+
+      @a = MINT::AIReference.first
+    end
+    it 'should return to defocused in case there is no referred object' do
+      @a.process_event(:organize)
+      @a.process_event(:present)
+      @a.process_event(:focus)
+      @a.states.should == [:defocused]
     end
   end
+
 end

d6e1a46f2bbd963fb97ae4753c9d7116ecba7b50
Sebastian Feuerstack <Sebastian@Feuerstack.org>
2012-06-29 09:10:04 -0300

    changed bindaction to support new pubsub client api

--- lib/MINT-core/mapping/action/bind_action.rb
+++ lib/MINT-core/mapping/action/bind_action.rb
@@ -19,7 +19,7 @@ class BindAction < Action
   end
 
   def elementOut
-      @action[:elementOut]
+    @action[:elementOut]
   end
 
   def identifier
@@ -52,36 +52,24 @@ class BindAction < Action
 
 
   def start(observation_results)            # TODO handle observation_variables
-    RedisConnector.sub.subscribe(channelIn).callback do
-      @cb_observation_has_subscribed.call(elementIn) if @cb_observation_has_subscribed
-    end
+    redis = RedisConnector.redis
+    redis.pubsub.subscribe(channelIn) { |message|
 
 
-    # This does not work, because claues are not stored in data model unless the streaming is stopped
-    #e_class = MINT::Interactor.class_from_channel_name elementIn
+      found=JSON.parse message
 
-    # element = e_class.first(:name=>nameIn)
-    #d = element.attribute_get(@action[:attrIn])
+      if nameIn.eql? found['name']
 
-    #RedisConnector.pub.publish @action[:elementOut], {:name=>@action[:nameOut], @action[:attrOut] => d}.to_json
+        if found.has_key? @action[:attrIn]
+          result =  found[@action[:attrIn]]
+          result = @action[:transform].call result if @action[:transform]
+          RedisConnector.pub.publish channelOut, {:name=>@action[:nameOut], @action[:attrOut] => result}.to_json
 
-
-    RedisConnector.sub.on(:message) do |c, message|
-      if c.eql? channelIn
-        p c
-
-        found=JSON.parse message
-
-        if nameIn.eql? found['name']
-
-          if found.has_key? @action[:attrIn]
-            result =  found[@action[:attrIn]]
-            result = @action[:transform].call result if @action[:transform]
-            RedisConnector.pub.publish channelOut, {:name=>@action[:nameOut], @action[:attrOut] => result}.to_json
-
-          end
         end
+
       end
+    }.callback do
+      @cb_observation_has_subscribed.call(elementIn) if @cb_observation_has_subscribed
     end
     self
   end

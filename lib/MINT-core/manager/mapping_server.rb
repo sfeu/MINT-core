cb1c8555f329dde62718b5c106bc21468a958acd
Sebastian Feuerstack <Sebastian@Feuerstack.org>
2012-07-25 14:55:35 -0300

    implemented filtering

--- lib/MINT-core/manager/mapping_server.rb
+++ lib/MINT-core/manager/mapping_server.rb
@@ -14,12 +14,28 @@ class MappingServer
     attr_accessor :manager
 
     def initialize
+      @mapping_store = {}
       super()
     end
 
     def forward_callback(mapping,data)
       p "in forward #{mapping} #{data}"
-      send_data(mapping+"|"+data.to_json+"\r\n")
+
+      if (data[:result])
+        d = data[:result]
+        if not @mapping_store[mapping] and d.first and d.first[1]['name']
+          @mapping_store[mapping] =  {:name => d.first[1]['name']}
+        end
+      end
+      if data[:mapping_state]
+        if data[:mapping_state].eql? :succeeded
+          send_data("STATUS|"+mapping+"|"+@mapping_store[mapping].to_json+"\r\n")
+          p "sent: STATUS|"+mapping+"|"+@mapping_store[mapping].to_json
+        end
+        @mapping_store[mapping]=nil
+      end
+
+
     end
 
     def receive_line(data)
@@ -31,6 +47,9 @@ class MappingServer
           when "REGISTER"
             @manager.register_callback(d[1], method(:forward_callback))
             send_data("REGISTERED|#{d[1]}\r\n")
+          when "LIST"
+            mappings = @manager.get_mappings
+            mappings.each {|m| send_data("MAPPING|#{m}\r\n")}
           else
             p "ERROR\r\nReceived Unknown data:#{data}\r\n "
         end
@@ -48,19 +67,19 @@ class MappingServer
 
   def start(manager = nil)
 
-      if (manager)
-        @manager = manager
-      else
-        @manager = MappingManager.new
-      end
-      EventMachine::start_server @host, @port, StatefulProtocol do |conn|
-        @connections << conn
-        conn.send_data("READY\r\n")
-        conn.manager=@manager
-        puts "connection..."
-      end
-      puts "Started server on #{@host}:#{@port}"
+    if (manager)
+      @manager = manager
+    else
+      @manager = MappingManager.new
     end
+    EventMachine::start_server @host, @port, StatefulProtocol do |conn|
+      @connections << conn
+      conn.send_data("READY\r\n")
+      conn.manager=@manager
+      puts "connection..."
+    end
+    puts "Started server on #{@host}:#{@port}"
+  end
 
 end
 

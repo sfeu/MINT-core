a99b2894f755e7f13d6420c1bbcb8aa8a64b9306
Sebastian Feuerstack <Sebastian@Feuerstack.org>
2012-07-25 15:01:05 -0300

    added action states to inform about general success of a set of actions

--- lib/MINT-core/mapping/mapping.rb
+++ lib/MINT-core/mapping/mapping.rb
@@ -26,6 +26,11 @@ module MINT
       @observation_results = {}
 
       @observation_state = {}
+
+      @action_states = {}
+
+      #overall action sucecces used for direct callback
+      @actions_success = false
     end
 
     def mapping_name
@@ -51,15 +56,24 @@ module MINT
 
     def startAction(observation_results)
       @action_init = 0
+      @actions_success = false
       actions.each do |action|
         @action_init += 1
         @state_callback.call(@mapping[:name], {:id => action.id, :state => :activated}) if @state_callback
-        action.start(observation_results).finished_callback {
-          @state_callback.call(@mapping[:name], {:id => action.id, :state => :succeeded}) if @state_callback
+        action.start(observation_results).finished_callback { |id,result|
+          state = :failed
+          state = :succeeded if result
+
+          @action_states[id]=state
+
+          @state_callback.call(@mapping[:name], {:id => action.id, :state => state}) if @state_callback
           @action_init -= 1
           if @action_init == 0
             @action_activated = true
-            call_actions_succeeded_callbacks
+            r = false
+            r = true if not @action_states.values.include? :failed
+            @actions_success = r
+            call_actions_succeeded_callbacks r
           end
         }   # pass observation variables
       end
@@ -95,7 +109,7 @@ module MINT
       return unless block
 
       if @action_activated
-        block.call(self)
+        block.call(self,@actions_success )
       else
         @actions_succeeded_callbacks ||= []
         @actions_succeeded_callbacks.unshift block # << block
@@ -103,23 +117,24 @@ module MINT
     end
 
 
-    def call_actions_succeeded_callbacks
+    def call_actions_succeeded_callbacks(result)
       @actions_succeeded_callbacks ||= []
 
       while cb = @actions_succeeded_callbacks.pop
-        cb.call(self)
+        cb.call(self,result)
       end
       @actions_succeeded_callbacks.clear if @actions_succeeded_callbacks
     end
 
     # function is called every time an observation has been fulfilled or failed (in_state == :fail)
     def cb_activate_action(element,in_state,result,id)
-      @state_callback.call(@mapping[:name], {:id => id, :state => in_state.to_s.to_sym}) if @state_callback
+      @state_callback.call(@mapping[:name], {:id => id, :element => element, :result => result, :state => in_state.to_s.to_sym}) if @state_callback
 
       @observation_state[element] = in_state
 
       if in_state == :fail
         stop_observations # unsubscribe observations
+        @state_callback.call(@mapping[:name], {:id => @mapping[:id], :mapping_state => :failed}) if @state_callback
         restart # restart mapping
         return
       end
@@ -129,8 +144,11 @@ module MINT
         if not @observation_state.values.include? false
           stop_observations # unsubscribe observations
 
-          startAction(@observation_results).actions_succeeded_callback { |m|
-            @state_callback.call(@mapping[:name], {:id => @mapping[:id], :mapping_state => :finished}) if @state_callback
+          startAction(@observation_results).actions_succeeded_callback { |m,result|
+            state = :failed
+            state = :succeeded if result
+
+            @state_callback.call(@mapping[:name], {:id => @mapping[:id], :mapping_state => state}) if @state_callback
 
             # TODO -clean restart and move check_at_startup inside observation
             # - add callback for action if succeeded & implement parallel action execution
@@ -143,6 +161,8 @@ module MINT
 
     def restart
       @observation_state = {}
+      @action_states = {}
+
       observations.each do |observation|
         @observation_state[observation.element] = false
         observation.start

f84478a1015db970009860237b78d3b0439e655a
Sebastian Feuerstack <Sebastian@Feuerstack.org>
2012-08-02 15:54:41 -0300

    removed synchronization calls that we implement as mappings seperately changed listed to unchosen to be more clear

--- lib/MINT-core/model/aui/AIChoiceElement.rb
+++ lib/MINT-core/model/aui/AIChoiceElement.rb
@@ -1,50 +1,5 @@
 module MINT
-  class AIChoiceElement_sync_callback < AIO_sync_callback
-     def initialize(element)
-       @element=element
-     end
-     def sync_cio_to_listed
-       true
-     end
-
-     def sync_cio_to_selected
-       true
-     end
-
-     def unchoose_others
-       aios = []
-       @element.parent.children.each do |c|
-         if c.is_in? :chosen
-           aios << c
-         end
-       end
-       aios.each do |aio|
-         aio.process_event("unchoose")
-       end
-       true
-     end
-   end
-
    class AIChoiceElement < AIINDiscrete
-
-     def sync_event(event)
-       process_event(event, AIChoiceElement_sync_callback.new(self))
-     end
-     def sync_cio_to_selected
-       cio =  MINT::Selectable.first(:name=>self.name)
-       if (cio and not cio.is_in? :selected)
-         cio.sync_event(:select)
-       end
-       true
-     end
-
-     def sync_cio_to_listed
-       cio =  MINT::Selectable.first(:name=>self.name)
-       if (cio and not cio.is_in? :listed)
-         cio.sync_event(:listed)
-       end
-       true
-     end
 
      def unchoose_others
        aios = []

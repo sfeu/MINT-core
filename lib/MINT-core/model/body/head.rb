72362193a95b3151444798d608f285dc71086483
Sebastian Feuerstack <Sebastian@Feuerstack.org>
2012-08-03 15:31:04 -0300

    fixed transitions

--- lib/MINT-core/model/body/head.rb
+++ lib/MINT-core/model/body/head.rb
@@ -4,16 +4,9 @@ module MINT
     #property :angle, Integer, :default  => -1
     #property :distance, Integer, :default  => -1
 
-    def initialize_statemachine
-
-      if @statemachine.blank?
-
-        parser = StatemachineParser.new(self)
-        @statemachine = parser.build_from_scxml "#{File.dirname(__FILE__)}/head.scxml"
-        @statemachine.reset
-      end
-    end
-
+       def getSCXML
+          "#{File.dirname(__FILE__)}/head.scxml"
+        end
 
 
     def angle=(angle)

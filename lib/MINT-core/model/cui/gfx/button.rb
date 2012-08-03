05f67121fd1b7851ef329cb739ce6cb033f6eb4e
Sebastian Feuerstack <Sebastian@Feuerstack.org>
2012-08-03 15:30:40 -0300

    added inital_js state

--- lib/MINT-core/model/cui/gfx/button.rb
+++ lib/MINT-core/model/cui/gfx/button.rb
@@ -1,17 +1,6 @@
 
 module MINT
 
-  class Button_sync_callback < CIO_sync_callback
-    def sync_aio_to_activated
-      true
-    end
-
-    def sync_aio_to_deactivated
-      true
-    end
-
-  end
-
   class Button < CIO
 
     def getModel
@@ -22,25 +11,6 @@ module MINT
           "#{File.dirname(__FILE__)}/button.scxml"
     end
 
-    def sync_event(event)
-      process_event(event, Button_sync_callback.new)
-    end
 
-    def sync_aio_to_activated
-      aio =  MINT::AICommand.first(:name=>self.name)
-      if (aio and not aio.is_in? :activated)
-        aio.sync_event(:activate)
-      end
-      true
-    end
-
-
-    def sync_aio_to_deactivated
-      aio =  MINT::AICommand.first(:name=>self.name)
-      if (aio and not aio.is_in? :deactivated)
-        aio.sync_event(:deactivate)
-      end
-      true
-    end
   end
 end
